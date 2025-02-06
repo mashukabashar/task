a06ba9ed276c966bb9fec6833821becf60c36951
mashukabashar <diyamoni422@gmail.com>
2025-02-07 00:03:05 +0600

    module-5

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -15,8 +15,8 @@ class Task(models.Model):
         on_delete=models.CASCADE, 
         default=1)
     
-    assigned_to = models.ManyToManyField(Employee)
-    
+    assigned_to = models.ManyToManyField(Employee, related_name="tasks")
+
     # notun_string=models.CharField(max_length=100,default="")
     title = models.CharField(max_length=250)
     description=models.TextField()
@@ -39,7 +39,7 @@ class TaskDetail(models.Model):
         (LOW, 'Low')
     )
 
-    task=models.OneToOneField(Task, on_delete=models.CASCADE)
+    task=models.OneToOneField(Task, on_delete=models.CASCADE, related_name="details")
 
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
