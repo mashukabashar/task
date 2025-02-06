5d7f33d028a83b768809721c7c6612b66e5f620e
mashukabashar <diyamoni422@gmail.com>
2025-02-06 10:34:35 +0600

    6 feb

--- task-management/tasks/views.py
+++ task-management/tasks/views.py
@@ -3,16 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Welcome to the task management system")
+# def home(request):
+#     return HttpResponse("Welcome to the task management system")
 
-def contact(request):
-    return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the contact page</h1>")
+# def contact(request):
+#     return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the contact page</h1>")
 
-def show_task(request):
-    return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the task page</h1>")
+# def show_task(request):
+#     return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the task page</h1>")
 
-def show_specific_task(request, id):
-    return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the specific task page</h1>")
-print("id",id)
-print("id type",type(id))
+# def show_specific_task(request, id):
+#     return HttpResponse("<h1 style='color:red; text-align:center'>Welcome to the specific task page</h1>")
+# print("id",id)
+# print("id type",type(id))
+
+
+def manager_dashboard(request):
+    return render(request,"dashboard/manager-dashboard.html")
+
+def user_dashboard(request):
+    return render(request,"dashboard/user-dashboard.html")
+
+def test(request):
+    return render(request,"test.html")
